4253927cdd3be952dc3f8b6c14ac979284f327c3
renatocorreia-rmcm <rmcm@cin.ufpe.br>
2026-04-18 13:47:56 -0300

    deploy

---
+++ evaluate_polynomial.py
@@ -0,0 +1,19 @@
+import numpy as np
+import numpy.typing as npt
+
+
+def my_pow(x: float, e: int):
+    r = 1
+    for i in range(e): r*=x
+    return r
+
+
+def evaluate_polynomial(coefficients: npt.NDArray[float], x: float):
+
+    result = 0
+
+    for deg, coef in enumerate(coefficients):
+        if coef != 0:
+            result += coef * my_pow(x, deg)
+
+    return result

33723cbe57c5df061af179cd4b5759c6e6753afb
renatocorreia-rmcm <rmcm@cin.ufpe.br>
2026-04-26 22:13:50 -0300

    optimized vandermond matrix setting

--- vandermond.py
+++ vandermond.py
@@ -20,15 +20,24 @@ def coefficients(
 
     T: npt.NDArray[float] = np.zeros(shape=(amount_of_coefficients, amount_of_coefficients), dtype=float)
 
+    # setting T by vectorized approach T_ij = (t_i)^j
+    exponents = np.arange(amount_of_coefficients)
+    T = t[:, None] ** exponents
+
     # setting T by collumn
+    """
     for index_column in range(amount_of_coefficients):
         T[:, index_column] = t ** index_column
 
+    """
+
     # setting T by line
-    # exponents = np.arange(amount_of_coefficients)
-    # print(exponents)
-    # for line in range(amount_of_coefficients):
-    #     T[line] = interpolation_points[line, 0] ** exponents
+    """
+    exponents = np.arange(amount_of_coefficients)
+    print(exponents)
+    for line in range(amount_of_coefficients):
+        T[line] = interpolation_points[line, 0] ** exponents
+    """
 
     T_inv = householder.inversion(T)
 
